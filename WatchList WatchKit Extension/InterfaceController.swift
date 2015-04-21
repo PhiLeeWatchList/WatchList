ad7dfb12ace15fbd0d7d0270f3cd9fb19a074cd7
pbamma <philipstarner@hotmail.com>
2015-04-21 15:03:21 -0700

    watchList now  building and simulating.

---
+++ WatchList WatchKit Extension/InterfaceController.swift
@@ -0,0 +1,31 @@
+//
+//  InterfaceController.swift
+//  WatchList WatchKit Extension
+//
+//  Created by Phil Starner on 4/21/15.
+//  Copyright (c) 2015 PhiLee. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awakeWithContext(context: AnyObject?) {
+        super.awakeWithContext(context)
+        
+        // Configure interface objects here.
+    }
+
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
