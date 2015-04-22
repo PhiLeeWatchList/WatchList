ab5f618a5041f815086b46afc75db7f4fbe6a376
pbamma <philipstarner@hotmail.com>
2015-04-22 14:18:36 -0700

    reconfigured with navigation view controller as main.  This should suffice for now so as to get us working on functionality.

--- WatchList/BeaconConfigViewController.swift
+++ WatchList/BeaconConfigViewController.swift
@@ -1,25 +1,28 @@
 //
-//  ViewController.swift
+//  BeaconConfigViewController.swift
 //  WatchList
 //
-//  Created by Phil Starner on 3/17/15.
+//  Created by Phil Starner on 4/22/15.
 //  Copyright (c) 2015 PhiLee. All rights reserved.
 //
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class BeaconConfigViewController: UIViewController {
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        
+        
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
+    
 }
-
---
+++ WatchList/MainViewController.swift
@@ -0,0 +1,29 @@
+//
+//  MainViewController.swift
+//  WatchList
+//
+//  Created by Phil Starner on 4/22/15.
+//  Copyright (c) 2015 PhiLee. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.navigationController?.navigationBar.barStyle = UIBarStyle.Black;
+        
+        
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+    
+}
