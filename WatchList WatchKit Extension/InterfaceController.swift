443e80b5922c82baaa67d37209ab60d92a9e4903
Lee Strasheim <lstrasheim@mac.com>
2015-04-23 02:17:00 -0700

    added table to watch app to show users who arrived.  Notification alert includes button to see you else has arrived.

--- WatchList WatchKit Extension/InterfaceController.swift
+++ WatchList WatchKit Extension/InterfaceController.swift
@@ -12,10 +12,57 @@ import Foundation
 
 class InterfaceController: WKInterfaceController {
 
+    @IBOutlet weak var table: WKInterfaceTable!
+    
+    var users = [String]()
+    
+    var tableCreated = false
+    
+    func refreshTable() {
+        if tableCreated == false {
+            
+            table.setNumberOfRows(users.count, withRowType: "tableRowController")
+            
+            for (index, user) in enumerate(users) {
+                let row = table.rowControllerAtIndex(index) as! tableRowController
+                row.tableRowLabel.setText(user)
+            }
+            
+            tableCreated = true
+        }
+    }
+    
+    override func handleActionWithIdentifier(identifier: String?, forRemoteNotification remoteNotification: [NSObject : AnyObject]) {
+        if let notificationIdentifier = identifier {
+            let dateFormatter = NSDateFormatter()
+            dateFormatter.dateFormat = "hh:mm a"
+            let stringDate = dateFormatter.stringFromDate(NSDate())
+            
+            var user = notificationIdentifier + " arrived at " + stringDate
+            
+            
+            if NSUserDefaults.standardUserDefaults().objectForKey("users") != nil {
+                users = NSUserDefaults.standardUserDefaults().objectForKey("users") as! [String]
+            }
+            
+            users.append(user)
+            
+            NSUserDefaults.standardUserDefaults().setObject(users, forKey: "users")
+            
+            refreshTable()
+        }
+    }
+    
     override func awakeWithContext(context: AnyObject?) {
         super.awakeWithContext(context)
         
-        // Configure interface objects here.
+        if NSUserDefaults.standardUserDefaults().objectForKey("users") != nil {
+            users = NSUserDefaults.standardUserDefaults().objectForKey("users") as! [String]
+        } else {
+            users.append("No one has arrived yet")
+        }
+        
+        refreshTable()
     }
 
     override func willActivate() {
---
+++ WatchList/tableRowController.swift
@@ -0,0 +1,17 @@
+//
+//  tableRowController.swift
+//  WatchList
+//
+//  Created by Lee Strasheim on 4/23/15.
+//  Copyright (c) 2015 PhiLee. All rights reserved.
+//
+
+import UIKit
+import WatchKit
+
+class tableRowController: NSObject {
+   
+    @IBOutlet weak var tableRowLabel: WKInterfaceLabel!
+
+    
+}
