8c828124dd466c106f4f9326e601d52bff9cf39d
pbamma <philipstarner@hotmail.com>
2015-04-26 10:41:47 -0700

    removed major/minor... new email composition to include app url parameter link.

--- WatchList/BeaconConfigViewController.swift
+++ WatchList/BeaconConfigViewController.swift
@@ -16,13 +16,15 @@ class BeaconConfigViewController: UIViewController, MFMailComposeViewControllerD
     
     @IBOutlet weak var newButton: UIButton!
     @IBOutlet weak var guidText: UITextField!
-    @IBOutlet weak var majorText: UITextField!
-    @IBOutlet weak var minorText: UITextField!
+    @IBOutlet weak var firstText: UITextField!
+    @IBOutlet weak var lastText: UITextField!
     @IBOutlet weak var doneButton: UIButton!
     @IBOutlet weak var messageLabel: UILabel!
     
     var majorInt:UInt16 = 0
     var minorInt:UInt16 = 0
+    var first:String = ""
+    var last:String = ""
     var guid:String = "E2C56DB5-DFFB-48D2-B060-D0F5A71096E0"
     
     
@@ -44,15 +46,15 @@ class BeaconConfigViewController: UIViewController, MFMailComposeViewControllerD
         self.newButton.layer.borderColor = UIColor.init(red: 112.0/255.0, green: 146.0/255.0, blue: 255.0/255.0, alpha: 1).CGColor
         self.newButton.layer.borderWidth = 1.0
         
-        self.majorText.layer.cornerRadius = 6.0
-        self.majorText.layer.masksToBounds = true
-        self.majorText.layer.borderColor = UIColor.init(red: 112.0/255.0, green: 146.0/255.0, blue: 255.0/255.0, alpha: 1).CGColor
-        self.majorText.layer.borderWidth = 1.0
+        self.firstText.layer.cornerRadius = 6.0
+        self.firstText.layer.masksToBounds = true
+        self.firstText.layer.borderColor = UIColor.init(red: 112.0/255.0, green: 146.0/255.0, blue: 255.0/255.0, alpha: 1).CGColor
+        self.firstText.layer.borderWidth = 1.0
         
-        self.minorText.layer.cornerRadius = 6.0
-        self.minorText.layer.masksToBounds = true
-        self.minorText.layer.borderColor = UIColor.init(red: 112.0/255.0, green: 146.0/255.0, blue: 255.0/255.0, alpha: 1).CGColor
-        self.minorText.layer.borderWidth = 1.0
+        self.lastText.layer.cornerRadius = 6.0
+        self.lastText.layer.masksToBounds = true
+        self.lastText.layer.borderColor = UIColor.init(red: 112.0/255.0, green: 146.0/255.0, blue: 255.0/255.0, alpha: 1).CGColor
+        self.lastText.layer.borderWidth = 1.0
         
         self.doneButton.layer.cornerRadius = 6.0
         self.doneButton.layer.masksToBounds = true
@@ -79,7 +81,7 @@ class BeaconConfigViewController: UIViewController, MFMailComposeViewControllerD
         picker.mailComposeDelegate = self
         picker.setSubject("WatchList Friend ID")
         
-        picker.setMessageBody("Your friend whoever has sent you a WatchList friend code!  They must really like you! Copy these values into your friend list and then whoever will be able to see you around corners!!!!  Insane I know!!! <br><br> <b>GUID:</b> \(self.guid) <br> <b>Major:</b> \(self.majorInt) <br> <b>Minor:</b> \(self.minorInt)", isHTML: true)
+        picker.setMessageBody("Your friend \(self.firstText.text) has sent you a WatchList friend code!  They must really like you! Click the linke below from your iPhone mail and then \(self.firstText.text) will be able to see you around corners!!!!  Insane I know!!! <br><br> <a src=\"http://watchlist?udid=\(self.guid)&first=\(self.firstText.text)&last=\(self.lastText.text)\">\(self.firstText.text) \(self.lastText.text)</a> </b> <br> <br> <br>   full url: http://watchlist?udid=\(self.guid)&first=\(self.firstText.text)&last=\(self.lastText.text)", isHTML: true)
         
         presentViewController(picker, animated: true, completion: nil)
     }
@@ -89,12 +91,12 @@ class BeaconConfigViewController: UIViewController, MFMailComposeViewControllerD
         self.guidText.text = NSUUID().UUIDString;
         self.guid = self.guidText.text
         
-        self.majorInt = UInt16(arc4random_uniform(65535))
-        self.minorInt = UInt16(arc4random_uniform(65535))
-        
-        println("major: \(self.majorInt), minor \(self.minorInt)")
-        self.majorText.text = String(self.majorInt)
-        self.minorText.text = String(self.minorInt)
+//        self.majorInt = UInt16(arc4random_uniform(65535))
+//        self.minorInt = UInt16(arc4random_uniform(65535))
+//        
+//        println("major: \(self.majorInt), minor \(self.minorInt)")
+//        self.firstText.text = String(self.majorInt)
+//        self.lastText.text = String(self.minorInt)
     }
     
     
