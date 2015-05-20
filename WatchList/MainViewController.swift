57f0f76f54b7688b53d459682f260834f53b9aa3
pbamma <philipstarner@hotmail.com>
2015-05-19 17:41:19 -0700

    removed some BT Location code causing Lee problems.  changed login to Lee/Lee for test. moved map.

--- WatchList/MainViewController.swift
+++ WatchList/MainViewController.swift
@@ -83,9 +83,9 @@ class MainViewController: UIViewController, INBeaconServiceDelegate, CLLocationM
         
         // 34.153725, -118.336573 Morton's, Burbank CA
 
-        var latitude:CLLocationDegrees = 34.153725
+        var latitude:CLLocationDegrees = 34.159725
         
-        var longitude:CLLocationDegrees = -118.336573
+        var longitude:CLLocationDegrees = -118.331573
         
         var latDelta:CLLocationDegrees = 0.01
         
@@ -317,9 +317,9 @@ class MainViewController: UIViewController, INBeaconServiceDelegate, CLLocationM
             pinView = MKAnnotationView(annotation: annotation, reuseIdentifier: reuseId)
             pinView!.canShowCallout = true
             
-            var imageBorderColor:UIColor = UIColor(red: 186.0/255.0, green: 207.0/255.0, blue: 255.0/255.0, alpha: 1)
+            var imageBorderColor:UIColor = UIColor(red: 0/255.0, green: 0/255.0, blue: 0/255.0, alpha: 1)
             var imageBgColor:UIColor = UIColor(red: 40.0/255.0, green: 0.0/255.0, blue: 221.0/255.0, alpha: 1)
-            let tempSize:CGFloat = 30.0
+            let tempSize:CGFloat = 40.0
             
             var newUserImageView:UIImageView = UIImageView(frame: CGRectMake(0, 0, tempSize, tempSize))
             newUserImageView.layer.cornerRadius = newUserImageView.frame.size.width / 2
@@ -327,7 +327,7 @@ class MainViewController: UIViewController, INBeaconServiceDelegate, CLLocationM
             newUserImageView.layer.borderColor = imageBorderColor.CGColor
             newUserImageView.layer.backgroundColor = imageBgColor.CGColor
             newUserImageView.layer.borderWidth = tempSize/8
-            newUserImageView.image = UIImage(data: self.getUserImageData("chris")!)
+            newUserImageView.image = UIImage(named: "logo")//UIImage(data: self.getUserImageData("chris")!)
             
             var newUserView:UIView = UIView(frame: CGRectMake(0,0, userBubbleSize, userBubbleSize+labelSize))
             newUserView.addSubview(newUserImageView)
@@ -381,79 +381,79 @@ class MainViewController: UIViewController, INBeaconServiceDelegate, CLLocationM
     
     
     func service(service: INBeaconService!, foundDeviceUUID uuid: String!, withRange range: INDetectorRange) {
-        var textFieldString:String = ""
-        var nameString:String = self.whoHaveYouFound(uuid.uppercaseString)
-        var sendNotification:Bool = false
-        
-        
-        println("found \(uuid) \(range)")
-        let myRange = range.value
-        switch myRange {
-        case 0:
-             println("unknown")  //it looks like this essentially means "no detection"
-        case 1:
-            println("far")
-            textFieldString = "I found \(nameString) within 60ft!"
-            sendNotification = true
-        case 2:
-            println("near")
-            textFieldString = "I found \(nameString) within 5ft!"
-            sendNotification = true
-        case 3:
-            println("immediate")
-            textFieldString = "I found \(nameString) within 1ft!"
-            sendNotification = true
-        default:
-            println("Something else")
-        }
-        
-        self.messageLabel.text = textFieldString
-        
-        //send a notification if friend is detected.
-        if(self.canAddUserToField(uuid)) {
-            var context = CoreDataStack.sharedInstance.managedObjectContext!
-            let request = NSFetchRequest(entityName: "User")
-            let predicate = NSPredicate(format: "selected == true")
-            request.predicate = predicate
-            
-            if let users = context.executeFetchRequest(request, error: nil) as? [User] {
-                for user in users {
-                    nameString = user.username
-                    var imageData = user.image
-                    self.notificationSent = true
-                    let notification: UILocalNotification = UILocalNotification()
-                    
-                    notification.alertBody = "\(nameString) is here!!!"
-                    notification.soundName = UILocalNotificationDefaultSoundName
-                    /*
-                    If the application is in the foreground, it will get a callback to application:didReceiveLocalNotification:.
-                    If it's not, iOS will display the notification to the user.
-                    */
-                    UIApplication.sharedApplication().presentLocalNotificationNow(notification)
-                    self.addUserToView(nameString, imageData:imageData)
-                    personAddedArray.append(uuid)
-                    
-                }
-            }
-            
-//            let defaults = NSUserDefaults.standardUserDefaults()
-//            //let nameString = defaults.stringForKey(GlobalConstants.THIS_DEVICE_TRANSMIT_NAME)
-//            
-//            self.notificationSent = true
-//            let notification: UILocalNotification = UILocalNotification()
-//            
-//            notification.alertBody = "\(nameString) is here!!!"
-//            notification.soundName = UILocalNotificationDefaultSoundName
-//            /*
-//            If the application is in the foreground, it will get a callback to application:didReceiveLocalNotification:.
-//            If it's not, iOS will display the notification to the user.
-//            */
-//            UIApplication.sharedApplication().presentLocalNotificationNow(notification)
-//            self.addUserToView(nameString)
-//            personAddedArray.append(uuid)
-        }
-//        if(sendNotification && !self.notificationSent) {
+//        var textFieldString:String = ""
+//        var nameString:String = self.whoHaveYouFound(uuid.uppercaseString)
+//        var sendNotification:Bool = false
+//        
+//        
+//        println("found \(uuid) \(range)")
+//        let myRange = range.value
+//        switch myRange {
+//        case 0:
+//             println("unknown")  //it looks like this essentially means "no detection"
+//        case 1:
+//            println("far")
+//            textFieldString = "I found \(nameString) within 60ft!"
+//            sendNotification = true
+//        case 2:
+//            println("near")
+//            textFieldString = "I found \(nameString) within 5ft!"
+//            sendNotification = true
+//        case 3:
+//            println("immediate")
+//            textFieldString = "I found \(nameString) within 1ft!"
+//            sendNotification = true
+//        default:
+//            println("Something else")
 //        }
+//        
+//        self.messageLabel.text = textFieldString
+//        
+//        //send a notification if friend is detected.
+//        if(self.canAddUserToField(uuid)) {
+//            var context = CoreDataStack.sharedInstance.managedObjectContext!
+//            let request = NSFetchRequest(entityName: "User")
+//            let predicate = NSPredicate(format: "selected == true")
+//            request.predicate = predicate
+//            
+//            if let users = context.executeFetchRequest(request, error: nil) as? [User] {
+//                for user in users {
+//                    nameString = user.username
+//                    var imageData = user.image
+//                    self.notificationSent = true
+//                    let notification: UILocalNotification = UILocalNotification()
+//                    
+//                    notification.alertBody = "\(nameString) is here!!!"
+//                    notification.soundName = UILocalNotificationDefaultSoundName
+//                    /*
+//                    If the application is in the foreground, it will get a callback to application:didReceiveLocalNotification:.
+//                    If it's not, iOS will display the notification to the user.
+//                    */
+//                    UIApplication.sharedApplication().presentLocalNotificationNow(notification)
+//                    self.addUserToView(nameString, imageData:imageData)
+//                    personAddedArray.append(uuid)
+//                    
+//                }
+//            }
+//            
+////            let defaults = NSUserDefaults.standardUserDefaults()
+////            //let nameString = defaults.stringForKey(GlobalConstants.THIS_DEVICE_TRANSMIT_NAME)
+////            
+////            self.notificationSent = true
+////            let notification: UILocalNotification = UILocalNotification()
+////            
+////            notification.alertBody = "\(nameString) is here!!!"
+////            notification.soundName = UILocalNotificationDefaultSoundName
+////            /*
+////            If the application is in the foreground, it will get a callback to application:didReceiveLocalNotification:.
+////            If it's not, iOS will display the notification to the user.
+////            */
+////            UIApplication.sharedApplication().presentLocalNotificationNow(notification)
+////            self.addUserToView(nameString)
+////            personAddedArray.append(uuid)
+//        }
+////        if(sendNotification && !self.notificationSent) {
+////        }
         
     }
     
@@ -744,14 +744,14 @@ class MainViewController: UIViewController, INBeaconServiceDelegate, CLLocationM
         //        txtLongitude.text = "\(location.coordinate.longitude)";
         
         //attempt to start up beacon detection and broadcast
-//        INBeaconService.singleton().removeDelegate(self)
-        INBeaconService.singleton().stopBroadcasting()
-        INBeaconService.singleton().stopDetecting()
-        
-//        INBeaconService.singleton().addDelegate(self)
-        INBeaconService.singleton().startDetecting()
-//
-        INBeaconService.singleton().startBroadcasting()
+////        INBeaconService.singleton().removeDelegate(self)
+//        INBeaconService.singleton().stopBroadcasting()
+//        INBeaconService.singleton().stopDetecting()
+//        
+////        INBeaconService.singleton().addDelegate(self)
+//        INBeaconService.singleton().startDetecting()
+////
+//        INBeaconService.singleton().startBroadcasting()
     }
     
     func locationManager(manager: CLLocationManager!, didFailWithError error: NSError!) {
